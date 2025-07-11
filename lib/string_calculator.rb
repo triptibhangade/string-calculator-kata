ec845a730465d1d754a829cd8842897645469099
Tripti <tripti.bhangade@gmail.com>
2025-07-11 12:50:53 +0530

    Support custom single-character delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,13 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    numbers.split(/,|\n/).map(&:to_i).sum
+
+    delimiter = /,|\n/
+    if numbers.start_with?("//")
+      delimiter_line, numbers = numbers.split("\n", 2)
+      delimiter = Regexp.escape(delimiter_line[2])
+    end
+
+    numbers.split(/#{delimiter}/).map(&:to_i).sum
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -22,5 +22,9 @@ RSpec.describe StringCalculator do
     it 'handles newlines as delimiters along with commas' do
       expect(StringCalculator.new.add("1\n2,3")).to eq(6)
     end
+
+    it 'supports custom delimiters like ;' do
+      expect(StringCalculator.new.add("//;\n1;2")).to eq(3)
+    end
   end
 end
