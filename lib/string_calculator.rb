6b338e333ac50f424dbfa90be48fb0e84fe4094f
Tripti <tripti.bhangade@gmail.com>
2025-07-12 10:57:22 +0530

    Modified the method

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,15 +2,9 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    delimiter = /,|\n/
-    if numbers.start_with?("//")
-      delimiter_line, numbers = numbers.split("\n", 2)
-      delimiter = Regexp.escape(delimiter_line[2])
-    end
+    nums = numbers.scan(/-?\d+/).map(&:to_i)
 
-    nums = numbers.split(/#{delimiter}/).map(&:to_i)
     negatives = nums.select { |n| n < 0 }
-
     unless negatives.empty?
       raise "negative numbers not allowed #{negatives.join(",")}"
     end
